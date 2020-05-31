3237e3654ed86bdb2912d845fb40c343a220b3b0
summerLilyGone <2317957806@qq.com>
2020-05-31 21:06:14 +0800

    添加登陆配置

--- MyQQGame/assets/Script/Framework/core/Config.js
+++ MyQQGame/assets/Script/Framework/core/Config.js
@@ -4,6 +4,10 @@ var Config = cc.Class({
 
     statics: {
         //模拟登陆http的地址
-        maomaoHttp : "http//:maomao.com",
+        resgisterUrl : "http://106.14.223.175:10000/root/gateway.action?command=user@createUser&userName=%s&password=%s",
+        //模拟登陆gate的地址
+        gateUrl : "http://106.14.223.175:10000/root/gateway.action?command=user@login&userName=%s&password=%s",
+        //登陆服
+        loginUrl : "http://106.14.223.175:11000/root/gateway.action?command=player@getPlayerInfo&session=%s",
     },
 });
