90a02867fefe72e12791e43981f74ca3c999c471
unknown <guojw@hariogame.com>
2020-08-11 00:58:11 +0800

    本地数据实装

--- MyQQGame/assets/Script/Framework/core/UserDataMgr.js
+++ MyQQGame/assets/Script/Framework/core/UserDataMgr.js
@@ -37,7 +37,7 @@ var UserDataMgr = cc.Class({
 				};
 				for (var i = 1; i <= ChapterData[1].cities[k].missionNum; i++){
 					id = i >= 10 && i.toString() || ("0" + i.toString());
-					missionId = ChapterData[1].countryId + ChapterData[1].cities[k].cityId + id;
+					var missionId = ChapterData[1].countryId + ChapterData[1].cities[k].cityId + id;
 					var isOpen = k == 1 && i == 1;
 					cityData.mission[i] = {
 						index : i,
@@ -64,34 +64,36 @@ var UserDataMgr = cc.Class({
 
 	//保存本地数据
 	SaveuserData:function(){
+		console.log("保存本地数据", this.userData);
 		var userData = this.userData;
 		cc.sys.localStorage.setItem('game_userData',JSON.stringify(userData));
 	},
 
 	//更新本地数据
 	UpdateuserData: function(missionId, star){
-		for (var k1 in this.userData){
-			for (var k2 in this.userData[k1].mission){
-				if (this.userData[k1].mission[k2].missionId == missionId){
-					if (this.userData[k1].mission[k2].star == 0 && star > 0){
-						var nextData = this.userData[k1].mission[k2 + 1];
-						if (nextData){
+		console.log("更新本地数据", missionId, star);
+		for (var k1 in this.userData.missionData){
+			for (var k2 in this.userData.missionData[k1].mission){
+				if (this.userData.missionData[k1].mission[k2].missionId == missionId){
+					if (this.userData.missionData[k1].mission[k2].star == 0 && star > 0){
+						var nextData = this.userData.missionData[k1].mission[Number(k2) + 1];
+						if (nextData != null){
 							///下一关开启
-							this.userData[k1].mission[k2 + 1].isOpen = true;
+							this.userData.missionData[k1].mission[Number(k2) + 1].isOpen = true;
 						}
 						else
 						{
 							///下一城市开启
-							this.userData[k1 + 1].isOpen = true;
-							this.userData[k1 + 1].mission[1].isOpen = true;
+							this.userData.missionData[Number(k1) + 1].isOpen = true;
+							this.userData.missionData[Number(k1) + 1].mission[1].isOpen = true;
 						}
 					}
 					///如果通关星数大于记录则修改星星数
-					if (star > this.userData[k1].mission[k2].star){
+					if (star > this.userData.missionData[k1].mission[k2].star){
 						///更新城市总星星数
-						this.userData[k1].star += star - this.userData[k1].mission[k2].star;
+						this.userData.missionData[k1].star += star - this.userData.missionData[k1].mission[k2].star;
 						///更新关卡星星数
-						this.userData[k1].mission[k2].star = star
+						this.userData.missionData[k1].mission[k2].star = star
 					}
 					break;
 				}
